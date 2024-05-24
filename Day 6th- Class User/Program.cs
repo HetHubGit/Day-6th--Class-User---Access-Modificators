aa8048cba24774d16ed53a813f091431a8306a45
HetHubGit <164913849+HetHubGit@users.noreply.github.com>
2024-05-24 13:28:17 +0100

    Remember - Class'es must be always in separate files

--- Day 6th- Class User/Program.cs
+++ Day 6th- Class User/Program.cs
@@ -1,4 +1,6 @@
-﻿User user1 = new User("Jacek", "Pass738");
+﻿using Day_6th__Class_User;
+
+User user1 = new User("Jacek", "Pass738");
 User user2 = new User("Olga", "Pass738");
 User user3 = new User("Daniel", "Pass738");
 User user4 = new User("Julia", "Pass738");
@@ -8,47 +10,3 @@ user1.AddScore(5);
 user1.AddScore(2);
 var result = user1.Result;
 Console.WriteLine(result);
-var name = User.GameName;
-var pi = Math.PI;
-
-
-class User
-{
-    public static string GameName = "Lokomotive";
-    private List<int> score = new List<int>();
-   
-   
-    public User(String login, String password)
-    {  
-        this.Login = login;
-        this.Password = password;
-        
-    }
-    public string Login { get; private set; }
-
-    public string Password { get; private set; }
-    public int Result 
-    {
-        get 
-        {
-            return this.score.Sum();
-        } 
-    }
-
-
-    public void AddScore(int number)
-    {
-       // this.score += number; // this.score = this.score + number - to samo tylko skrócone.
-       this.score.Add(number);
-    }
-
-
-
-
-
-
-    private static void Main(string[] args)
-    {
-       
-    }
-}
---
+++ Day 6th- Class User/User.cs
@@ -0,0 +1,40 @@
+﻿namespace Day_6th__Class_User
+{
+    public class User
+    {
+        
+        private List<int> score = new List<int>();
+
+
+        public User(String login, String password)
+        {
+            this.Login = login;
+            this.Password = password;
+
+        }
+        public string Login { get; private set; }
+
+        public string Password { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+
+        public void AddScore(int number)
+        {
+            // this.score += number; // this.score = this.score + number - to samo tylko skrócone.
+            this.score.Add(number);
+        }
+
+
+
+
+
+
+        
+    }
+}
