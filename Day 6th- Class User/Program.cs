0ed38633957031bb706c22bdd411338a45602767
HetHubGit <164913849+HetHubGit@users.noreply.github.com>
2024-05-27 11:48:11 +0100

    Day 7th-Including Tests

--- Day 6th- Class User/Program.cs
+++ Day 6th- Class User/Program.cs
@@ -3,7 +3,7 @@
 User user1 = new User("Jacek", "Pass738");
 User user2 = new User("Olga", "Pass738");
 User user3 = new User("Daniel", "Pass738");
-User user4 = new User("Julia", "Pass738");
+
 
 
 user1.AddScore(3);
--- Day 6th- Class User/User.cs
+++ Day 6th- Class User/User.cs
@@ -2,18 +2,13 @@
 {
     public class User
     {
-        
         private List<int> score = new List<int>();
-
-
         public User(String login, String password)
         {
             this.Login = login;
             this.Password = password;
-
         }
         public string Login { get; private set; }
-
         public string Password { get; private set; }
         public int Result
         {
@@ -22,19 +17,10 @@
                 return this.score.Sum();
             }
         }
-
-
         public void AddScore(int number)
         {
             // this.score += number; // this.score = this.score + number - to samo tylko skrócone.
-            this.score.Add(number);
+            this.score.Add(number);            
         }
-
-
-
-
-
-
-        
     }
 }
